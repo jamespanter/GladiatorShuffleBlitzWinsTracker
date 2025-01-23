ccd4d856fb73c3bde524e43938a3243e37884a29
James Panter <60343279+jamespanter@users.noreply.github.com>
2025-01-23 17:41:44 +0000

    Add achievement ids for TWW season 2

--- GladiatorShuffleBlitzWinsTracker_AchievementIDs.lua
+++ GladiatorShuffleBlitzWinsTracker_AchievementIDs.lua
@@ -9,13 +9,16 @@ AchievementIDs = {
         [36] = 19091, -- Gladiator: Dragonflight Season 3
         [37] = 19490, -- Gladiator: Dragonflight Season 4
         [38] = 40393, -- Gladiator: The War Within Season 1
+        [39] = 41032, -- Gladiator: The War Within Season 2
     },
     ShuffleLegend = {
         [36] = 19304, -- Legend: Dragonflight Season 3
         [37] = 19500, -- Legend: Dragonflight Season 4
         [38] = 40395, -- Legend: The War Within Season 1
+        [39] = 41358, -- Legend: The War Within Season 2
     },
     BlitzStrategist = {
         [38] = 40233, -- Strategist: The War Within Season 1
+        [39] = 41363, -- Strategist: The War Within Season 2
     },
 }
